00b04aa7d9d147dc19e69c7e3b2c1ac3649cfbd1
Alaa Alkhatib <alaa.alkhatib@infostrategic.com>
2021-05-04 11:18:39 +0300

    create theme switcher component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import Articles from "./containers/Articles"
+import ThemeSwitcher from "./containers/ThemeSwitcher"
 
 import './styles/main.css';
 // need npm install  @material-ui/core before import MaterialUI
@@ -9,25 +10,20 @@ import redTheme from './themes/redTheme';
 import darkTheme from './themes/darkTheme';
 import alaaTheme from './themes/alaaTheme';
 import { GREEN, RED, BLUE, ALAACOLOR } from './constants/ThemeColors';
-import Button from '@material-ui/core/Button';
 
+// for  redux state and actions / store 
 import { connect } from 'react-redux';
-import { setDarkTheme, setThemeColor } from './store/Theme/themeActions'
-import { useEffect } from 'react';
 
-const App = ({ settings, setDarkTheme, setThemeColor }) => {
-  //this is come after mapStateToProps
-  console.log("App settings from  store ===", settings)
-  let { themeColor, isDarkTheme } = { isDarkTheme: settings.darkTheme, themeColor: settings.themeColor };
-  let applyTheme;
-  if (localStorage.getItem("themeColor")) {
-    console.log("stored theme is=========", localStorage.getItem("themeColor"))
-  }
-
+const App = ({themeSettings}) => {
+//this is come after mapStateToProps
+console.log("App settings from  store ===", themeSettings)
+let { themeColor } = { themeColor: themeSettings.themeColor };
+  
   //applyTheme= createMuiTheme(alaaTheme);
-  if (isDarkTheme) {
+  /* if (isDarkTheme) {
     applyTheme = createMuiTheme(darkTheme)
-  } else {
+  } else { */
+    let applyTheme;
     console.log("set theme template=========", themeColor)
     switch (themeColor) {
       case GREEN: {
@@ -48,74 +44,20 @@ const App = ({ settings, setDarkTheme, setThemeColor }) => {
         }
 
     }
-  }
-useEffect(()=>{
-  // call one time to set theme from local storage
-  console.log("useEffect one time====")
-  const body = document.body.classList;
-  if (!isDarkTheme) {
-    body.remove(themeColor);
-    body.add(themeColor);
-  }
-  else {
-    body.toggle(themeColor);
-  }
-},[])
-  
+  /* } */
 
-
-  const handleThemeColor = async (colorCode, i) => {
-    console.log(colorCode, "colorcode");
-    setThemeColor(colorCode);
-    const body = document.body.classList;
-    body.remove(themeColor);
-    body.remove("dark-"+themeColor);
-    body.add(colorCode);
-  };
-  const handleDarkTheme = async (isDark) => {
-    setDarkTheme(isDark);
-    const body = document.body.classList;
-    body.toggle((isDark ? "dark-" : "") + themeColor);
-  };
   return (
     <MuiThemeProvider theme={applyTheme}>
       <div className="App">
         <div>
-          
-          <div>
-            <button onClick={() => handleDarkTheme(true)}>Activate Dark Theme</button>
-          </div>
-          <button style={{ color: "red" }} onClick={() => handleThemeColor(RED)}>Red Theme</button>
-          <button style={{ color: "blue" }} onClick={() => handleThemeColor(BLUE)}>Blue Theme</button>
-          <button style={{ color: "green" }} onClick={() => handleThemeColor(GREEN)}>Green Theme</button>
-          <button style={{ color: "gray" }} onClick={() => handleThemeColor(ALAACOLOR)}>Alaa Theme</button>
+          {/* component for switching theme with redux store */}
+          <ThemeSwitcher />
         </div>
         <header  >
           <img src={logo} className="App-logo" alt="logo" />
           <Articles />
         </header>
-        <div>
 
-        </div>
-        <br></br>
-        <div>
-            {/* this is materialui Button component  themed by MuiThemeProvider 
-               every change on theme color will effect these buttons
-            */}
-            <Button variant="contained">Default</Button>
-            <Button variant="contained" color="primary">
-              Primary
-            </Button>
-            <Button variant="contained" color="secondary">
-              Secondary
-            </Button>
-            <Button variant="contained" disabled>
-              Disabled
-            </Button>
-            <Button variant="contained" color="primary" href="#contained-buttons">
-              Link
-            </Button>
-          </div>
       </div>
 
     </MuiThemeProvider>
@@ -125,16 +67,10 @@ useEffect(()=>{
 const mapStateToProps = statefromstore => {
   console.log("App=== Global State Store ======", statefromstore)
   const { themeColor, darkTheme } = statefromstore.theme;
-  return { settings: { themeColor, isDarkTheme: darkTheme } }
+  return { themeSettings: { themeColor, isDarkTheme: darkTheme } }
   // very very important name returned here should be the same in  
   // function defention
-  // const App = ({settings,setDarkTheme,setThemeColor}) => {
-  //return settings
+  // const App = ({themeSettings}) => {
+  //return themeSettings
 };
-export default connect(mapStateToProps,
-  {
-    setDarkTheme,
-    setThemeColor
-  })
-  (App);
-//export default App;
+export default connect(mapStateToProps)(App) 
